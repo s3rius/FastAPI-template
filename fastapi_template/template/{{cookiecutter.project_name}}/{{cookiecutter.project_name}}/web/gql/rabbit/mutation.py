9247bf151c85b5a26b3f5beb69c5782584a1d618
Pavel Kirilin <win10@list.ru>
2022-05-04 03:40:20 +0400

    Added RabbitMQ service.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

---
+++ fastapi_template/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/web/gql/rabbit/mutation.py
@@ -0,0 +1,35 @@
+import strawberry
+from strawberry.types import Info
+
+from {{cookiecutter.project_name}}.web.gql.context import Context
+from {{cookiecutter.project_name}}.web.gql.rabbit.schema import RabbitMessageDTO
+from aio_pika import Message
+
+
+@strawberry.type
+class Mutation:
+    """Mutation for rabbit package."""
+
+    @strawberry.mutation(description="Send message to RabbitMQ")
+    async def send_rabbit_message(
+        self, message: RabbitMessageDTO, info: Info[Context, None]
+    ) -> None:
+        """
+        Send a message in RabbitMQ.
+
+        :param message: message to publish.
+        :param info: current context.
+        """
+        async with info.context.rabbit.acquire() as conn:
+            exchange = await conn.declare_exchange(
+                name=message.exchange_name,
+                auto_delete=True,
+            )
+            await exchange.publish(
+                message=Message(
+                    body=message.message.encode("utf-8"),
+                    content_encoding="utf-8",
+                    content_type="text/plain",
+                ),
+                routing_key=message.routing_key,
+            )
