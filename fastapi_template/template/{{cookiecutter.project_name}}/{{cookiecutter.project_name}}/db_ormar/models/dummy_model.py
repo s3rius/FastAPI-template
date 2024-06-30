22b147ec464fe71768c218c6b8b55d5d0c3a2a3a
Pavel Kirilin <win10@list.ru>
2024-06-30 12:04:03 +0200

    Dependencies were updated.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- fastapi_template/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/db_ormar/models/dummy_model.py
+++ fastapi_template/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/db_ormar/models/dummy_model.py
@@ -1,12 +1,10 @@
 import ormar
-from {{cookiecutter.project_name}}.db.base import BaseMeta
+from {{cookiecutter.project_name}}.db.base import ormar_config
 
 
 class DummyModel(ormar.Model):
     """Model for demo purpose."""
-
-    class Meta(BaseMeta):
-        tablename = "dummy_model"
+    ormar_config = ormar_config.copy(tablename="dummy_model")
 
     id: int = ormar.Integer(primary_key=True)
     name: str = ormar.String(max_length=200)  # noqa: WPS432
