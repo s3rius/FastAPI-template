00ff6d55ad39f69b3dec676dcaf395893c2b40ba
Pavel Kirilin <win10@list.ru>
2025-11-28 21:27:45 +0100

    Bumped deps versions.

--- scripts/version_bumper.py
+++ scripts/version_bumper.py
@@ -1,13 +1,12 @@
 import argparse
 from pathlib import Path
-from typing import List, Optional
 import re
 import requests
+import random
+from packaging.version import Version
 
-RAW_VERSION_RE = re.compile(r'(?P<package>.*)\s*=\s*\"(?P<version>[\^\~\>\=\<\!]?[\d\.\-\w]+)\"')
-EXPANDED_VER_RE = re.compile(
-    r'(?P<package>.*)\s*=\s*\{(.*)version\s*=\s*\"(?P<version>[\^\~\>\=\<\!]?[\d\.\-\w]+)\"(.*)\}'
-)
+DEP_RE = re.compile(r"\s*\"(?P<package>.+)\s*(?P<constraints>\>\=.+,\<.+)\s*\"")
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -15,74 +14,48 @@ def parse_args() -> argparse.Namespace:
         "file",
         type=Path,
     )
-    parser.add_argument(
-        "--section",
-        "-s",
-        type=str,
-        default="tool.poetry.dependencies",
-    )
     return parser.parse_args()
 
-def get_dependencies(path: Path, section: str) -> List[str]:
-    read_file = path.read_text()
-    recording = False
-    deps = []
-    for index, line in enumerate(read_file.splitlines(keepends=False)):
-        if line.startswith('[') and line.strip('[]') != section:
-            recording = False
-            continue
-        if line == f"[{section}]":
-            recording = True
-            continue
-        if line.startswith('python ='):
-            continue
-        if line.startswith('{%'):
-            continue
-        if recording:
-            deps.append((index, line))
-    return deps
 
-def get_new_version(package_name: str) -> Optional[str]:
-    resp = requests.get(f'https://pypi.org/pypi/{package_name}/json')
-    if not resp.ok:
-        return None
+def get_new_version(package_name: str) -> Version:
+    name = package_name.split("[")[0]
+    print("Fetching new version for", name)
+    resp = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+    resp.raise_for_status()
     rjson = resp.json()
-    return rjson['info']["version"]
+    return Version(rjson["info"]["version"])
 
-
-def bump_version(dependency: str) -> str:
-    exp_match = EXPANDED_VER_RE.match(dependency)
-    raw_match = None
-    if exp_match:
-        package = exp_match.group("package").strip()
-        version = exp_match.group("version").lstrip("^=!~<>")
-    else:
-        raw_match = RAW_VERSION_RE.match(dependency)
-    if raw_match:
-        package = raw_match.group("package").strip()
-        version = raw_match.group("version").lstrip("^=!~<>")
-    if exp_match is None and raw_match is None:
-        return None
-
-    print(f"Checking {package}")
-    new_version = get_new_version(package)
-    if new_version is not None and version != new_version:
-        print(f"Found new version: {new_version}")
-        return dependency.replace(version, new_version)
-
-    return None
 
 def main():
     args = parse_args()
-    deps = get_dependencies(args.file, args.section)
-    lines = args.file.read_text().splitlines(keepends=False)
-    for i, dep in deps:
-        new_version = bump_version(dep)
-        if new_version:
-            lines[i] = new_version
-    args.file.write_text("\n".join(lines))
-    
-
+    file: Path = args.file
+    lines = []
+    known_versions = {}
+    with file.open("r") as f:
+        for line in f:
+            match = DEP_RE.match(line)
+            if not match:
+                lines.append(line)
+                continue
+            full_package = match.group("package").strip()
+            package = full_package.strip().split("[")[0]
+            if package in known_versions:
+                print("Using known version for", package)
+                version = known_versions[package]
+            else:
+                version = get_new_version(package)
+                known_versions[package] = version
+            upper_version = version.major + 1
+            constraints = match.group("constraints").strip().split(";")
+            markers = None
+            if len(constraints) == 2:
+                markers = constraints[1]
+            new_constraints = f">={version},<{upper_version}"
+            if markers:
+                new_constraints += f"; {markers}"
+            lines.append('  "' + full_package + " " + new_constraints + '"\n')
+    with file.open("w") as f:
+        f.writelines(lines)
 
 if __name__ == "__main__":
     main()
